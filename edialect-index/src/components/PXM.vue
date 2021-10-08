1a73e7eb8e003a0de5554937db620b0930794643
sheeplin <1270610465@qq.com>
2021-10-09 02:34:11 +0800

    feat: 更新莆仙方言信息

---
+++ edialect-index/src/components/PXM.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-btn
+          depressed
+          @click="goToNewWebsite('https://pxm.edialect.top')"
+      >
+        点击访问莆仙方言公共服务包：兴化语记
+        <v-icon>
+          mdi-arrow-right
+        </v-icon>
+      </v-btn>
+    </v-row>
+    <v-row>
+      <div>
+        莆仙方言，又称兴化语、兴化话、兴化方言、莆仙语，当地人又称之为本地话，莆田讲的本地话称为莆田话，仙游县讲的本地话也称为仙游话，是分布于福建沿海中东部的地方语言。莆仙话分布于莆田市辖区（荔城区、涵江区、城厢区、秀屿区、乌丘屿）、仙游县以及泉州市泉港区、福清市的部分地区，在莆仙地区和旁边的使用人口约500万，因历史上该地区属于兴化军、兴安州、兴化府，故莆仙话又称为兴化话。以莆仙话为载体的非物质文化遗产有莆仙戏和莆仙木偶戏等。
+      </div>
+    </v-row>
+    <v-row>
+      <v-img :src="fenbu" alt="莆仙方言分布图"/>
+    </v-row>
+
+  </v-container>
+</template>
+
+<script>
+const cdn = 'https://hinghwadict-1259415432.cos.ap-shanghai.myqcloud.com/website/'
+
+export default {
+  name: "PXM",
+  data() {
+    return {}
+  },
+  computed: {
+    fenbu() {
+      return cdn + '莆仙方言分布图.png'
+    }
+  },
+  methods: {
+    goToNewWebsite(url) {
+      window.open(url)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- edialect-index/src/views/Home.vue
+++ edialect-index/src/views/Home.vue
@@ -1,120 +1,109 @@
 <template>
-	<div>
-		<v-container>
-			<v-card>
-				<v-row>
-					<v-col cols="4">
-						<v-card-title class="white--text text-h5" style="background-color:#93B372">
-							汉语方言分类
-						</v-card-title>
-						<v-sheet class="pa-4" style="background-color:#93B372">
-							<v-text-field
-								v-model="search"
-								clear-icon="mdi-close-circle-outline"
-								clearable
-								dark
-								flat
-								hide-details
-								label="检索你想查看的方言"
-								solo-inverted
-							></v-text-field>
-						</v-sheet>
-						<v-card-text>
-							<v-treeview
-								:active.sync="active"
-								:filter="filter"
-								:items="dialectList"
-								:search="search"
-								activatable
-								hoverable
-								open-all
-								open-on-click
-								rounded
-							>
-							</v-treeview>
-						</v-card-text>
-					</v-col>
-					<v-col cols="8">
-						<v-card>
-							<v-card-title>
-								{{ dialect.name }}
-							</v-card-title>
+  <div>
+    <v-container>
+      <v-card>
+        <v-row>
+          <v-col cols="4">
+            <v-card-title class="white--text text-h5" style="background-color:#93B372">
+              汉语方言分类
+            </v-card-title>
+            <v-sheet class="pa-4" style="background-color:#93B372">
+              <v-text-field
+                  v-model="search"
+                  clear-icon="mdi-close-circle-outline"
+                  clearable
+                  dark
+                  flat
+                  hide-details
+                  label="检索你想查看的方言"
+                  solo-inverted
+              ></v-text-field>
+            </v-sheet>
+            <v-card-text>
+              <v-treeview
+                  :active.sync="active"
+                  :filter="filter"
+                  :items="dialectList"
+                  :search="search"
+                  activatable
+                  hoverable
+                  open-all
+                  open-on-click
+                  rounded
+              >
+              </v-treeview>
+            </v-card-text>
+          </v-col>
+          <v-col cols="8">
+            <v-card>
+              <v-card-title>
+                {{ dialect.name }}
+              </v-card-title>
 
-							<v-card-text v-if="dialect.name!=='请选择'">
+              <v-card-text v-if="dialect.name!=='请选择'">
 
-								<v-divider class="pa-3"></v-divider>
-								<p>
-									{{ dialect.content }}
-								</p>
-								<v-btn
-									v-if="dialect.link"
-									depressed
-									@click="goToNewWebsite(dialect.link)"
-								>
-									开始访问
-									<v-icon>
-										mdi-arrow-right
-									</v-icon>
-								</v-btn>
-							</v-card-text>
-						</v-card>
-					</v-col>
-				</v-row>
-			</v-card>
+                <v-divider class="pa-3"></v-divider>
+                <PXM v-if="dialect.name==='莆仙方言'"/>
+                <p v-else>
+                  {{ dialect.content }}
+                </p>
 
-		</v-container>
-	</div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card>
+
+    </v-container>
+  </div>
 </template>
 
 <script>
-import { dialects } from '@/assets/content.js'
+import {dialects} from '../assets/content.js'
+import PXM        from "../components/PXM";
 
 export default {
-	name: 'Home',
-	data() {
-		return {
-			active: ['莆仙方言'],
-			search: null,
-		}
-	},
-	computed: {
-		filter() {
-			return (item, search) => {
+  name: 'Home',
+  components: {PXM},
+  data() {
+    return {
+      active: ['莆仙方言'],
+      search: null,
+    }
+  },
+  computed: {
+    filter() {
+      return (item, search) => {
 
-				for (let i in item)
-					if (item[i].indexOf(search) > -1){
-						return true
-					}
-				return false
-			}
-		},
+        for (let i in item)
+          if (item[i].indexOf(search) > -1) {
+            return true
+          }
+        return false
+      }
+    },
 
-		dialect() {
-			let dialect = undefined
-			if (this.active)
-				dialect = this.active[0]
-			let result = { name: '请选择' }
-			if (dialect) {
-				this.dialectList.forEach(item => {
-					if (item.id === dialect) return result = item
-					if (item.children) {
-						item.children.forEach(jtem => {
-							if (jtem.id === dialect) return result = jtem
-						})
-					}
-				})
-			}
-			return result
-		},
-		dialectList() {
-			return dialects
-		}
+    dialect() {
+      let dialect = undefined
+      if (this.active)
+        dialect = this.active[0]
+      let result = {name: '请选择'}
+      if (dialect) {
+        this.dialectList.forEach(item => {
+          if (item.id === dialect) return result = item
+          if (item.children) {
+            item.children.forEach(jtem => {
+              if (jtem.id === dialect) return result = jtem
+            })
+          }
+        })
+      }
+      return result
+    },
+    dialectList() {
+      return dialects
+    }
 
-	},
-	methods: {
-		goToNewWebsite(url) {
-			window.open(url)
-		}
-	}
+  }
 }
 </script>
